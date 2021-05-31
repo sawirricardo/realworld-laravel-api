6785a87d7bfe2b97f359a99e86cbdc764eee61e6
ricardosawir <sawir.ricardo@gmail.com>
2021-05-31 17:25:06 +0700

    fixed seeding data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Article;
 use App\Models\Comment;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -31,6 +32,17 @@ class DatabaseSeeder extends Seeder
 
         foreach (\App\Models\Article::all() as $article) {
             $article->tags()->sync(range(1, 8));
+            $article->favoriters()->sync(range(1, 8));
+        }
+
+        $users = \App\Models\User::all();
+        foreach ($users as $user) {
+            DB::table('followers')->insert([
+                'user_id' => $user->id,
+                'follower_id' => $users->filter(function ($userModel) use ($user) {
+                    return $userModel->id !== $user->id;
+                })->random()->id,
+            ]);
         }
     }
 }
